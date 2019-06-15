e87f6f006bd2ceb1da3033d6fe275fc8224f6e71
aksJain0 <aakshay1294@gmail.com>
2019-06-15 14:04:40 +0530

    fibonacci_huge and fibonacci_sum_last_digit coded

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,5 +1,6 @@
 # Uses python3
 import sys
+import random
 
 def get_fibonacci_huge_naive(n, m):
     if n <= 1:
@@ -13,7 +14,31 @@ def get_fibonacci_huge_naive(n, m):
 
     return current % m
 
+def get_fibonacci_huge(n, m):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % m
+
+    return current
+
 if __name__ == '__main__':
+
+    # while 1 : 
+    #     n = random.randrange(1000)
+    #     m = random.randrange(100) + 1
+    #     fn = get_fibonacci_huge_naive(n,m)
+    #     f =get_fibonacci_huge(n,m)
+    #     if(fn == f):
+    #         print("OK!")
+    #     else:
+    #         print(n,m,"NOT OK!!")
+    #         break
+
     input = sys.stdin.read();
     n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    print(get_fibonacci_huge(n, m))
--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,5 +1,6 @@
 # Uses python3
 import sys
+import random
 
 def fibonacci_sum_naive(n):
     if n <= 1:
@@ -15,7 +16,29 @@ def fibonacci_sum_naive(n):
 
     return sum % 10
 
+
+def fibonacci_sum(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+    sum      = 1
+
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)%10
+        sum = (sum + current) %10
+
+    return sum
+
 if __name__ == '__main__':
+
+    # while(1):
+    #     n = random.randrange(1,100)
+    #     if(fibonacci_sum(n) != fibonacci_sum_naive(n) ):
+    #         print("NOT OK!")
+    #         break
+
     input = sys.stdin.read()
     n = int(input)
-    print(fibonacci_sum_naive(n))
+    print(fibonacci_sum(n))
