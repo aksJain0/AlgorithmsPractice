0ebf090941207b9ef8f573e9fd7370b4ccb82ef4
akshay <akshay.jain2@exlservice.com>
2019-06-21 13:49:03 +0000

    coded money change

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,19 @@
 # Uses python3
 import sys
+import math
 
 def get_change(m):
     #write your code here
-    return m
+    r = m
+    n = 0
+
+    for e in [10, 5, 1]:
+        if r == 0:
+            return n
+        nc = math.floor(r/e)
+        n = n + nc
+        r = r - ( e * nc )
+    return int(n)
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
