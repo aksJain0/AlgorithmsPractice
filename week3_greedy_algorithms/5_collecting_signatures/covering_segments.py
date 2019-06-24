89c50f0d3a678c9e3ecd95c0d8743093d78b9cfa
akshay <akshay.jain2@exlservice.com>
2019-06-24 16:09:30 +0000

    coded covering segment but buggy

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -5,18 +5,46 @@ from collections import namedtuple
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
-    points = []
+    points = set()
     #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
-    return points
+    srtd = sorted(segments)
+    # print(srtd)
+    # print(type(srtd))
+    current = 0
+    previous = 0
+    point = 1000000000  # 10^9
+    while current < len(srtd):
+        # print("c =", current)
+        # print(points)
+        if srtd[current].start == srtd[previous].start:
+            point = min(point,srtd[current].end)
+            previous = current 
+            current = current + 1
+        elif srtd[current].start > point:
+            points.add(point)
+            point = srtd[current].end
+            previous = current
+            current = current + 1
+        else:
+            #print("appending point")
+            points.add(point)
+            previous = current
+            current = current + 1
+            
+
+    return list(points)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
-    n, *data = map(int, input.split())
+    #print(input)
+    n, *data = (map(int, input.split()))
+    # n = all[0]
+    # data = all[1:]
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
     print(len(points))
     for p in points:
         print(p, end=' ')
+
+
+
