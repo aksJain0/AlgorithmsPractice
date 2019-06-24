304191f3ea61eb701abd7c9128dfb32feadb1b75
akshay <akshay.jain2@exlservice.com>
2019-06-24 12:27:43 +0000

    coded car fueling

--- week3_greedy_algorithms/3_car_fueling/car_fueling.py
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.py
@@ -4,8 +4,27 @@ import sys
 
 def compute_min_refills(distance, tank, stops):
     # write your code here
-    return -1
+    currentFill = 0 
+    numFills = 0
+    stops.insert(0,0)
+    stops.append(distance)
+    n = len(stops)
+    #print("stops",stops, n)
+    while currentFill < n-1 :
+        lastFill = currentFill
+        try:
+            while currentFill < n-1 and (stops[currentFill + 1] - stops[lastFill] <= tank):
+                currentFill += 1
+        except:
+            print("out of range",currentFill,lastFill, numFills)
+            
+        if currentFill == lastFill:
+            return -1
+        elif currentFill < n-1:
+            numFills += 1
+    return numFills
 
 if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
+    all =  list(map(int, sys.stdin.read().split()))
+    d, m, _, stops = all[0], all[1], all [2], all [3:]
     print(compute_min_refills(d, m, stops))
