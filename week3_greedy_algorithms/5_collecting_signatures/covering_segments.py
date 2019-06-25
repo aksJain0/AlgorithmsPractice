dd8497102f1d0c68ec56a4c12c306b6b27a00c2b
akshay <akshay.jain2@exlservice.com>
2019-06-25 09:43:05 +0000

    coded covering segmnets and examples in pdf working

--- week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
+++ week3_greedy_algorithms/5_collecting_signatures/covering_segments.py
@@ -4,35 +4,50 @@ from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
+# intersection finds the intersection of two segments
+def intersection(seg1, seg2):
+    # If any of the segmnet is None return the other segment
+    if seg1 is None :
+        return seg2
+    if seg2 is None :
+        return seg1
+    # takes two segments of named tuple segment and return the intersection part as a segment named tuple
+    start = max(seg1.start, seg2.start) # We took max first as we want to move foreward in number line
+    end = min(seg1.end, seg2.end)
+    if start <= end:
+        return Segment(start, end)
+    return None # Not able to form the intersection
+    
 def optimal_points(segments):
-    points = set()
     #write your code here
     srtd = sorted(segments)
     # print(srtd)
     # print(type(srtd))
-    current = 0
-    previous = 0
-    point = 1000000000  # 10^9
-    while current < len(srtd):
-        # print("c =", current)
-        # print(points)
-        if srtd[current].start == srtd[previous].start:
-            point = min(point,srtd[current].end)
-            previous = current 
-            current = current + 1
-        elif srtd[current].start > point:
-            points.add(point)
-            point = srtd[current].end
-            previous = current
-            current = current + 1
-        else:
-            #print("appending point")
-            points.add(point)
-            previous = current
-            current = current + 1
-            
+    ptr = 0
+    intr = None
+    interList = []
 
-    return list(points)
+    while ptr < len(srtd):
+        intrPrev = intr
+        intr = intersection(intr, srtd[ptr])
+        # print("PTR = ", ptr)
+        # print("INTR = ", intr)
+        # print("INTRLIST = ", interList)
+        if intr is None :
+            interList.append(intrPrev)
+            intr = srtd[ptr]
+        ptr += 1
+    interList.append(intr)
+    pts = []
+    for x in interList:
+        pts.append(x.end)
+    # print("PTS = ", pts)
+
+    return pts
+
+
+
+        
 
 if __name__ == '__main__':
     input = sys.stdin.read()
