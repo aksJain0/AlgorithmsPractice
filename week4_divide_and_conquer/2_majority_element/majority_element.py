fbe97576c40e376c16e8899b9864b7ce75bd64ec
akshay <akshay.jain2@exlservice.com>
2019-07-17 14:13:01 +0000

    coded majority element and passed pdf cases

--- week4_divide_and_conquer/2_majority_element/majority_element.py
+++ week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -7,6 +7,34 @@ def get_majority_element(a, left, right):
     if left + 1 == right:
         return a[left]
     #write your code here
+    mid = left + ((right-left) // 2)
+    ml = get_majority_element(a, left, mid)
+    mr = get_majority_element(a, mid+1, right)
+
+    if ml != -1:
+        count = 0
+        for e in a[left:right]:
+            # print("checking for ml = ", ml," in a[",left,right,"] against", e)
+            if e == ml:
+                count += 1
+        # print("count of ml = ", ml, "count = ", count, "checking against = ", (right-left)//2)
+        if count > (right - left)//2:
+            # print("Returning ml = ",ml)
+            return ml
+
+    if mr != -1:
+        count = 0
+        for e in a[left:right]:
+            # print("checking for mr = ", mr, " in a[",left,right,"] against" ,e)
+            if e == mr:
+                count += 1
+        # print("count of mr = ", mr, " count = ", count, "checking against = ", (right-left))
+        if count > (right - left)//2:
+            # print("returning mr = ", mr)
+            return mr    
+        
+
+
     return -1
 
 if __name__ == '__main__':
