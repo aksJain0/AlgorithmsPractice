a3165bfd7a643b86b893da78e8e4ab6ab132c877
akshay <akshay.jain2@exlservice.com>
2019-07-17 15:55:10 +0000

    coded 3 partition sorting not working

--- week4_divide_and_conquer/3_improving_quicksort/sorting.py
+++ week4_divide_and_conquer/3_improving_quicksort/sorting.py
@@ -4,7 +4,28 @@ import random
 
 def partition3(a, l, r):
     #write your code here
-    pass
+    x = a[l]
+    j = 1
+    k = j
+    cnt = 1
+    for i in range(l+1, r+1):
+        if a[i] < x :
+            if k == j:
+                j += 1
+                k = j
+                a[i], a[j] = a[j], a[i]
+            else:
+                k += 1
+                j += 1
+                a[k], a[j] = a[j], a[k]
+                a[i], a[j] = a[j], a[i]
+
+        elif a[i] == x:
+            k += 1
+            a[i], a[k] = a[k], a[i]          
+
+    a[l], a[j] = a[j], a[l]
+    return (j,k)
 
 def partition2(a, l, r):
     x = a[l]
@@ -23,9 +44,10 @@ def randomized_quick_sort(a, l, r):
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
     #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    #m = partition2(a, l, r)
+    j,k = partition3(a,l, r)
+    randomized_quick_sort(a, l, j-1);
+    randomized_quick_sort(a, k+1, r);
 
 
 if __name__ == '__main__':
